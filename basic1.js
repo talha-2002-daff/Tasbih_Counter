dad30b7d07e7d0485b6473de31b250a6cf45e876
talha-2002-daff <tbk17916@gmail.com>
2026-07-26 23:56:04 +0600

    Change in the HTML: Aded the reset button

--- basic1.js
+++ basic1.js
@@ -2,7 +2,9 @@ let number = Number(localStorage.getItem("tasbih")) || 0;
 
 const data = document.getElementById("count");
 
+
 data.innerText = number;
+
 
 function increment() {
 
@@ -12,16 +14,36 @@ function increment() {
 
     localStorage.setItem("tasbih", number);
 
-
     data.classList.remove("animate");
-
     void data.offsetWidth;
-
     data.classList.add("animate");
 
 
     if (navigator.vibrate) {
         navigator.vibrate(20);
     }
+}
 
+
+function resetCounter() {
+
+    const confirmReset = confirm("Are you sure you want to reset the Tasbih count?");
+
+    if (confirmReset) {
+
+        number = 0;
+
+        data.innerText = number;
+
+        localStorage.setItem("tasbih", number);
+
+    
+        data.classList.remove("animate");
+        void data.offsetWidth;
+        data.classList.add("animate");
+
+        if (navigator.vibrate) {
+            navigator.vibrate(50);
+        }
+    }
 }
