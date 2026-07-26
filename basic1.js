46ee227257994db0bce4f6a5a6af5e24bd594d55
talha-2002-daff <tbk17916@gmail.com>
2026-07-26 23:45:25 +0600

    Changes in the CSS for animation

--- basic1.js
+++ basic1.js
@@ -1,11 +1,27 @@
-let number=0;
+let number = Number(localStorage.getItem("tasbih")) || 0;
 
-const data= document.getElementById("count");
+const data = document.getElementById("count");
 
-function increment(){
+data.innerText = number;
+
+function increment() {
+
     number++;
-    data.innerText=number;
-   
+
+    data.innerText = number;
+
+    localStorage.setItem("tasbih", number);
+
+
+    data.classList.remove("animate");
+
+    void data.offsetWidth;
+
+    data.classList.add("animate");
+
+
+    if (navigator.vibrate) {
+        navigator.vibrate(20);
+    }
+
 }
-
-
